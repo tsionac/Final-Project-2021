c0ea84b3632d71e134d7d4659bb03f4da6c54632
Tsiona Cohen <tsionac@post.bgu.ac.il>
2021-04-30 13:40:31 +0300

    center and make login in card

--- projects/project-site/src/app/dash/dash.component.ts
+++ projects/project-site/src/app/dash/dash.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,OnDestroy, Directive} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
+
 
 @Component({
   selector: 'app-dash',
