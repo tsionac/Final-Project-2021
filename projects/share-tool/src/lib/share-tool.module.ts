492a6ac913822c71a78992a37c6935a8a715f326
sportalcraft <42spamtal42@gmail.com>
2021-05-28 23:02:40 +0300

    add ability to use smart-text-box directly

--- projects/share-tool/src/lib/share-tool.module.ts
+++ projects/share-tool/src/lib/share-tool.module.ts
@@ -23,7 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatFormFieldModule,
   ],
-  exports: [ShareToolComponent]
+  exports: [ShareToolComponent, SmartTextBoxComponent]
 })
 export class ShareToolModule { }
 
--- projects/share-tool/src/public-api.ts
+++ projects/share-tool/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/share-tool.service';
 export * from './lib/share-tool.component';
 export * from './lib/share-tool.module';
+export * from './lib/smart-text-box/smart-text-box.component';
