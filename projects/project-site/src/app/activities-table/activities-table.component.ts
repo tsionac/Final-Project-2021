0e124acc944c1912d1f5ac742a2e6698eab3a92d
Tsiona Cohen <tsionac@post.bgu.ac.il>
2021-06-07 23:29:50 +0300

    table fixes filters

--- projects/project-site/src/app/activities-table/activities-table.component.ts
+++ projects/project-site/src/app/activities-table/activities-table.component.ts
@@ -27,10 +27,6 @@ export class ActivitiesTableComponent {
   displayedColumns: string[];
   dataSource;
 
-
-
-
-
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
 
   constructor(private recordService:RecordService, private alert:AlertService) {
@@ -49,7 +45,7 @@ export class ActivitiesTableComponent {
    * load all the edits to the filtered array
    */
    saveCopy() {
-    // this.filteredActivities = [];
+    this.filteredActivities = [];
     this.ELEMENT_DATA.forEach(activity => this.filteredActivities.push(activity));
     this.dataSource =  new MatTableDataSource(this.filteredActivities);
   }
@@ -68,6 +64,46 @@ export class ActivitiesTableComponent {
       this.ELEMENT_DATA = activities;
       this.saveCopy();
     })
+  }
+
+  
+  /**
+   * filter the recods
+   * @param userID an optional user name
+   */
+   filter(userID:string, componentID:String) {
+
+    //reset previus filteres
+    this.saveCopy();
+
+    // apply new filters
+    this.filteredActivities = this.filteredActivities.filter((activity:Activity) => {
+      let ans:Boolean = true;
+
+      if (userID !== undefined && userID != ''){
+        //tere is a requested filter for user
+        ans = ans && (activity.userID == userID)
+      }
+
+      if (componentID !== undefined && componentID != ''){
+        //tere is a requested filter for componentID
+        ans = ans && (activity.componentID == componentID)
+      }
+
+      if (this.filterDateFrom !== undefined && this.filterDateFrom != ''){
+        //tere is a requested filter for actionID
+        ans = ans && (new Date(this.filterDateFrom).getTime() <= activity.editStart.getTime())
+      }
+
+      if (this.filterDateTo !== undefined && this.filterDateTo != ''){
+        //tere is a requested filter for actionID
+        ans = ans && (new Date(this.filterDateTo).getTime() >= activity.editEnd.getTime())
+      }
+      return ans;
+
+    }
+    );
+    this.dataSource =  new MatTableDataSource(this.filteredActivities);
   }
 
   
