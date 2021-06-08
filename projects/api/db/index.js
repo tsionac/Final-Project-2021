fbedfb8bf02b8ec869253d9187beec60c54d0ea9
Tsiona Cohen <tsionac@post.bgu.ac.il>
2021-06-08 16:36:41 +0300

    commit add db remote

--- projects/api/db/index.js
+++ projects/api/db/index.js
@@ -5,72 +5,79 @@ mongoose.Promise = global.Promise;
 
 const host = 'localhost';
 const hostPort = 27017;
-const DB_URI = `mongodb://${host}:${hostPort}`;
+// const DB_URI = `mongodb://${host}:${hostPort}`;
+const DB_URI = `mongodb+srv://user:souriRE10@clusterfinal.hgsmz.mongodb.net/test?retryWrites=true&w=majority`;
 
 const { Manager } = require('../db/modules');
 
 function connect() {
-  return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-    if(process.env.NODE_ENV === 'test') {
-      // the code is running as a test.
-      // we will not connect to the realse DB, but for a moack DB.
-      // This is done becasue testst that change the DB might fail the secind time they will be runned (trying to add an exsisting record)
-      // connectin to a mock DB will allow tests to run indefenetly.
+        if (process.env.NODE_ENV === 'test') {
+            // the code is running as a test.
+            // we will not connect to the realse DB, but for a moack DB.
+            // This is done becasue testst that change the DB might fail the secind time they will be runned (trying to add an exsisting record)
+            // connectin to a mock DB will allow tests to run indefenetly.
 
-      const Mockgoose = require('mockgoose').Mockgoose;
-      const mockgoose = new Mockgoose(mongoose);
+            const Mockgoose = require('mockgoose').Mockgoose;
+            const mockgoose = new Mockgoose(mongoose);
 
-      // build the schemes
-      mockgoose.prepareStorage().then(() => {
-        // connect to mongoose normally
-        doConnect(resolve, reject);
-      });
+            // build the schemes
+            mockgoose.prepareStorage().then(() => {
+                // connect to mongoose normally
+                doConnect(resolve, reject);
+            });
 
-    } else {
-      // run this under normal situation, and a real DB.
-      doConnect(resolve, reject);
-    }
-  })
+        } else {
+            // run this under normal situation, and a real DB.
+            doConnect(resolve, reject);
+        }
+    })
 };
 
 function close() {
-  return mongoose.disconnect();
+    return mongoose.disconnect();
 }
 
 function doConnect(resolve, reject) {
-  mongoose.connect(DB_URI, {useNewUrlParser: true, useUnifiedTopology:true})
-  .then((res, err) =>{
-    if (err) return reject(err);
+    mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((res, err) => {
+            if (err) return reject(err);
 
-    console.log('connected succesfully to mongoDB!');
+            console.log('connected succesfully to mongoDB!');
 
 
-    //make usre there is an admin to the system
-    let userID = 'Admin';
-    let companyID = 'Admin';
-    let password = '12345678';
+            //make usre there is an admin to the system
+            let userID = 'Admin';
+            let companyID = 'Admin';
+            let password = '12345678';
 
-    let newManager = new Manager( {
-      companyID,
-      userID,
-      password
-    });
+            let newManager = new Manager({
+                companyID,
+                userID,
+                password
+            });
 
-    // create new admin if does not already exsist
-    Manager.findOne( {userID}, function (err, result) {
-      if (err) {  console.log(`FATAL 1 : Admin was not set!`, e);  reject(e); }
-      if (!result) {
-        newManager.save().then( (adminDoc) => {
-          console.log(`The admin has beed successfully set :  ${adminDoc}`);
-          resolve();
-        }).catch((e) => { console.log(`FATAL 2 : Admin was not set!`, e);  reject(e);})
-      } else{
-        console.log(`Admin already exsists.... doing nothing.`);
-        resolve();
-      }
-    });
-  });
+            // create new admin if does not already exsist
+            Manager.findOne({ userID }, function(err, result) {
+                if (err) {
+                    console.log(`FATAL 1 : Admin was not set!`, e);
+                    reject(e);
+                }
+                if (!result) {
+                    newManager.save().then((adminDoc) => {
+                        console.log(`The admin has beed successfully set :  ${adminDoc}`);
+                        resolve();
+                    }).catch((e) => {
+                        console.log(`FATAL 2 : Admin was not set!`, e);
+                        reject(e);
+                    })
+                } else {
+                    console.log(`Admin already exsists.... doing nothing.`);
+                    resolve();
+                }
+            });
+        });
 };
 
 //this is here to prevent depection warnings
@@ -79,6 +86,6 @@ mongoose.set('useFindAndModify', false);
 
 
 module.exports = {
-  connect,
-  close
+    connect,
+    close
 };
